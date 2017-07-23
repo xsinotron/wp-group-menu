dfc13bd82c0ef95a4bb43f15623a300e16ce17a7
Alexis <alecollin@gmail.com>
2017-07-23 11:00:35 +0200

    debug
    mise en place des éléments manquants de titre des liens

--- views/menu.php
+++ views/menu.php
@@ -3,7 +3,11 @@
         <ul>
             <?php foreach($menus as $menu):?>
             <li>
-                <a href="<?=$menu->siteUrl; ?>" <?php if($menu->siteId == $siteID):?> class="current"<?php endif;?> <?php if($menu->siteTarget == "blank"):?> target="_blank"<?php endif;?> >
+                <a href="<?=$menu->siteUrl; ?>"
+               	    <?php if($menu->siteId == $siteID):?> class="current"<?php endif;?>
+             	    <?php if($menu->siteTarget == "blank"):?> target="_blank"<?php endif;?>
+             	    <?php if($menu->siteAlt != ""):?> title="<?=$menu->siteAlt;?>"<?php endif;?>
+                >
                     <span><?=$menu->siteName;?></span>
                 </a>
             </li>
@@ -25,8 +29,10 @@
                 <?php foreach($menus as $menu): ?>
                 <li>
                     <a href="<?=$menu->siteUrl; ?>"
-                        <?php if($menu->siteId == $siteID):?> class=current"<?php endif;?>
-                        <?php if($menu->siteTarget == "blank"):?> target="_blank"<?php endif;?> >
+                		 <?php if($menu->siteId == $siteID):?> class="current"<?php endif;?>
+                		 <?php if($menu->siteTarget == "blank"):?> target="_blank"<?php endif;?>
+                		 <?php if($menu->siteAlt != ""):?> title="<?=$menu->siteAlt;?>"<?php endif;?>
+                	>
                     <span><?=$menu->siteName;?></span>
                     </a>
                 </li>
